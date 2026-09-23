c310566ad62870ea93d02b7247b08c23056540f2
Anjuman Sharear <sharearanjuman1@gmail.com>
2026-09-23 22:11:04 +0600

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
   return (
     <div className="app">
-      <h1>Zentra</h1>
+      <Sidebar />
     </div>
   );
 }
